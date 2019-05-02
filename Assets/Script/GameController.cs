4850f7b1fce90e435959ce75a70e0438d31ce969
wkmkymt <wkmkymt@gmail.com>
2019-05-03 05:04:52 +0900

    [add] Event of Counting Items

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public UnityEngine.UI.Text scoreLabel;
+    public void Update ()
+    {
+        int count = GameObject.FindGameObjectsWithTag ("Item").Length;
+        scoreLabel.text = count.ToString ();
+    }
+}
